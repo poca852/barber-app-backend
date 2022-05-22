4a34f4c6ec98ae40975a36d5748e122dee035e53
david <cuspoca3d@gmail.com>
2022-05-22 10:05:09 -0600

    se agrega configuracion extra para el manejo de ssl de parte de postgres

--- database/config.js
+++ database/config.js
@@ -2,7 +2,13 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     logging: false,
-    native: false
+    native: false,
+    dialectOptions: {
+        ssl:{
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 module.exports = sequelize;
