cf6d83ca02a95775647db6fa65ebd34b5270c266
david <cuspoca3d@gmail.com>
2022-05-23 17:50:43 -0600

    relacion rol to user

--- models/index.js
+++ models/index.js
@@ -1,5 +1,13 @@
+// models
 const UserModel = require('./usuario');
+const Rolmodel = require('./rol');
 
+// relations
+Rolmodel.hasOne(UserModel, {foreignKey: 'id'});
+UserModel.belongsTo(Rolmodel);
+
+// models exports 
 module.exports = { 
-  UserModel
+  UserModel,
+  Rolmodel
 }
