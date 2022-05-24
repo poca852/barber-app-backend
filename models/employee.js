42ccb36d69c0cb67fd1ae67d637fa3fb3eb99554
RepasoHenry <devWarrior7@protonmail.com>
2022-05-23 20:44:32 -0500

    Agregue el nombre del modelo correctamente

--- models/employee.js
+++ models/employee.js
@@ -1,7 +1,7 @@
 const {DataTypes} = require('sequelize');
 const sequelize = require('../database/config');
 
-const EmployeeModel = sequelize.define('date', {
+const EmployeeModel = sequelize.define('employee', {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
