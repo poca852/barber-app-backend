a3d0c2d8643732d3ed16611827365e5818977e69
david <cuspoca3d@gmail.com>
2022-05-24 07:26:05 -0600

    comentario para que no se olvide volver la env correcta para produccion

--- database/config.js
+++ database/config.js
@@ -1,6 +1,7 @@
 const {Sequelize} = require('sequelize');
 const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 
+// TODO: aqui se debe cambiar a la variable de entorno de DATABASE_URL  PARA QUE PUEDA FUNCIONAR EN PRODUCCION
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
     logging: false,
     native: false,
