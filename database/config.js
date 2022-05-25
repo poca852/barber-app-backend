252fe32ce37e687083f3306accdbb5ec7899d574
david cuspoca <cuspoca3d@gmail.com>
2022-05-24 20:24:53 -0600

    se preparo el archivo database/config para poder subirlo a heroku

--- database/config.js
+++ database/config.js
@@ -1,17 +1,19 @@
 const {Sequelize} = require('sequelize');
-const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
+
+// Esto solo se usa en desarrollo de momento se comentara para subir a heroku
+// const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 
 // TODO: aqui se debe cambiar a la variable de entorno de DATABASE_URL  PARA QUE PUEDA FUNCIONAR EN PRODUCCION
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     logging: false,
     native: false,
-    //error de ssl
-    // dialectOptions: {
-    //     ssl:{
-    //         require: true,
-    //         rejectUnauthorized: false
-    //     }
-    // }
+    // RECORDAR: muy importante esto se debe dejar en produccion, en desarrollo se puede quitar.
+    dialectOptions: {
+        ssl:{
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 module.exports = sequelize;
