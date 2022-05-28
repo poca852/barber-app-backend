d58905f82e98ab06ff56e74a02559d0234604ad6
RepasoHenry <devWarrior7@protonmail.com>
2022-05-28 15:59:40 -0500

    validacion de arreglo en ruta purchaseOrder ok

--- routes/purchaseOrder.js
+++ routes/purchaseOrder.js
@@ -3,7 +3,7 @@ const {check} = require('express-validator');
 const validarCampos = require('../middlewares/validar-campos.js');
 const {addPurchaseOrder} = require('../controllers/purchaseOrder');
 
-router.post('/', [ //Validacion da error porque en un "arreglo" de objetos el body
+router.post('/', [
     check("arr.*.idUser").isUUID(),
     check("arr.*.idProduct").isUUID(),
     check("arr.*.quantity").isInt(),
