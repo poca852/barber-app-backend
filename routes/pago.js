b4d866c29f6ddad724d8568e69e87008c29023e2
Consu <consudiazc@gmail.com>
2022-05-29 11:45:41 -0300

    prueba mod pago

--- routes/pago.js
+++ routes/pago.js
@@ -1,7 +1,20 @@
-const{Router} = require("express")
-const {} = require("express-validator") 
-const {}= require("../helpers")
+const {Router}= require ("express")
+const {check} = require("express-validator")
+const {validarCampos}= require ("../helpers")
+const {getPago, postPago}= require("../controllers")
 
 
+const router= Router()
 
-const router = Router();
+router.get("./",[
+check(),
+validarCampos
+],
+getPago)
+
+
+router.post("./",[
+check(),
+validarCampos
+],
+postPago)
