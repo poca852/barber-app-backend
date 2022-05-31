989fec59e78b6f49230d487cbe622a6837081e8e
david <cuspoca3d@gmail.com>
2022-05-30 19:48:06 -0600

    add path de barber app a mercado pago

--- MercadoPago/payment.js
+++ MercadoPago/payment.js
@@ -8,11 +8,11 @@ const url = "https://api.mercadopago.com/checkout/preferences";
             payer_email: "test_user_11278172@testuser.com", //comprador de Mercado Pago de prueba
             items:cart, //<-- carrito [{title,description,picture_url,category_id,quantity,unit_price}]
             back_urls:{
-            succes:"/succes",
+            succes:"https://barber-app-henry.herokuapp.com/api/pago/confirmation",
             failure: "/failure",
             pending: "/pending"
             },
-            notification_url: "https://www.your-site.com"
+            notification_url: ""
         };
 
         try {
