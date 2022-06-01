fd777e9413734ab209b0b75a239d580cd316521b
david <cuspoca3d@gmail.com>
2022-06-01 16:50:42 -0600

    deploy heroku mp

--- database/config.js
+++ database/config.js
@@ -1,22 +1,22 @@
 const { Sequelize } = require("sequelize");
 
 // Esto solo se usa en desarrollo de momento se comentara para subir a heroku
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 // TODO: aqui se debe cambiar a la variable de entorno de DATABASE_URL  PARA QUE PUEDA FUNCIONAR EN PRODUCCION
-const conexion_local = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
+// const conexion_local = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
 
 //const sequelize = new Sequelize(process.env.DATABASE_URL, {
-const sequelize = new Sequelize(conexion_local, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     logging: false,
     native: false,
     // RECORDAR: muy importante esto se debe dejar en produccion, en desarrollo se puede quitar.
-   //  dialectOptions: {
-   //       ssl: {
-   //       require: true,
-   //       rejectUnauthorized: false,
-   //      },
-   //   },
+    dialectOptions: {
+         ssl: {
+         require: true,
+         rejectUnauthorized: false,
+        },
+     },
   }
 );
 
