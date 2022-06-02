712d2049afc089124fbf0700b88ffcf68798dc83
david <cuspoca3d@gmail.com>
2022-06-01 18:44:14 -0600

    solucion problema de usuario

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -3,23 +3,6 @@ const bcryptjs = require("bcryptjs");
 const { UserModel, Rolmodel, DateModel, EmployeeModel } = require("../models");
 
 const addUser = async (req = request, res = response) => {
-  const { email, password, name, phone, avatar, idRol } = req.body;
-
-<<<<<<< HEAD
-  try {
-    // encriptamos el password
-    const hash = bcryptjs.hashSync(password, 10);
-
-    // armamos el body
-    const data = {
-      email,
-      password: hash,
-      name,
-      phone,
-      avatar,
-      idRol,
-    };
-=======
     const { email, password, name, phone, avatar, rol } = req.body;
     
     try {
@@ -42,12 +25,10 @@ const addUser = async (req = request, res = response) => {
           avatar,
           idRol: rolModel.id
         }
->>>>>>> b604fea8580167460d6e31b5141b1441ed63d25c
 
     // insertamos en la base de datos el user
     const user = await UserModel.create(data);
 
-<<<<<<< HEAD
     res.json({
       ok: true,
       name: user.name,
@@ -65,48 +46,7 @@ const addUser = async (req = request, res = response) => {
   }
 };
 
-const addRol = async (req = request, res = response) => {
-  const { rol } = req.body;
-  try {
-    const newRol = await Rolmodel.create({ rol: rol });
-
-    res.status(201).json({
-      ok: true,
-      newRol,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      ok: false,
-      msg: "Hable con el administrador",
-    });
-  }
-};
-
 const getUsers = async (req = request, res = response) => {
-=======
-        // TODO: aqui pendiente por definir si el usuario tiene que confirmar su email
-
-        res.json({
-          ok: true,
-          name: user.name,
-          id: user.id,
-          email: user.email,
-          phone: user.phone,
-          rol: queryRol
-        })
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: "Hable con el administrador",
-        });
-    }
-};
-
-const getUsers = async(req = request, res = response) => {
->>>>>>> b604fea8580167460d6e31b5141b1441ed63d25c
   try {
     const users = await UserModel.findAll({
       attributes: ["id", "name", "email"],
@@ -162,15 +102,9 @@ const getUser = async (req = request, res = response) => {
   }
 };
 
-<<<<<<< HEAD
-const putUser = async (req = request, res = response) => {
-  const { id } = req.params;
-  const { password, ...resto } = req.body;
-=======
 const putUser = async(req = request, res = response) => {
   const {idUser} = req.params;
   const {id, state, google, password, ...resto} = req.body;
->>>>>>> b604fea8580167460d6e31b5141b1441ed63d25c
   try {
     if (password) {
       resto.password = bcryptjs.hashSync(password, 10);
@@ -193,13 +127,8 @@ const putUser = async(req = request, res = response) => {
 
     const user = await UserModel.update(resto, {
       where: {
-<<<<<<< HEAD
-        id,
-      },
-=======
         id: idUser
       }
->>>>>>> b604fea8580167460d6e31b5141b1441ed63d25c
     });
 
     res.status(201).json({
@@ -215,26 +144,14 @@ const putUser = async(req = request, res = response) => {
   }
 };
 
-<<<<<<< HEAD
-const deleteUser = async (req = request, res = response) => {
-  const { id } = req.params;
-  try {
-    const user = await UserModel.update(
-      { state: false },
-      {
-        where: {
-          id,
-        },
-=======
 const deleteUser = async(req = request, res = response) => {
   const {idUser} = req.params;
   try {
     const user = await UserModel.update({state: false}, {
       where: {
         id: idUser
->>>>>>> b604fea8580167460d6e31b5141b1441ed63d25c
       }
-    );
+    })
 
     res.status(201).json({
       ok: true,
@@ -254,10 +171,5 @@ module.exports = {
   getUsers,
   getUser,
   putUser,
-<<<<<<< HEAD
-  deleteUser,
-  addRol,
-=======
   deleteUser
->>>>>>> b604fea8580167460d6e31b5141b1441ed63d25c
 };
