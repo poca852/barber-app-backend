1ed5c3c139b724ab970bd626e68a1fc71d5f6d95
Consu <consudiazc@gmail.com>
2022-06-08 17:01:10 -0300

    Se borra lo del PDF

--- helpers/functionSendMail.js
+++ helpers/functionSendMail.js
@@ -1,7 +1,6 @@
 const nodemailer = require("nodemailer")
-const pdf = require("pdf-creator-node");
-const fs = require("fs");
-const path = require('path');
+
+
 
 
 const sendMail = async(name, email ,idPurchaseOrder, date_approved, transaction_amount, pruebaData)=>{
@@ -24,58 +23,9 @@ const sendMail = async(name, email ,idPurchaseOrder, date_approved, transaction_
    <p>HOLA ${name}<p>
    <p style= "color: red"> Tu compra se ha realizado con exito!! Para mas información clickea aqui 👇: </p>
    <a href="${url}"> ${url}</a>
-   <p>Te adjuntamos el detalle de la orden de compra<p>
+
   `;
   
-
-  const options = {
-    format: "A3",
-    orientation: "portrait",
-    color: "red",
-    border: "10mm",
-    header: {
-        height: "45mm",
-        contents: '<div style="text-align: center;">BARBER APP</div>'
-    },
-    footer: {
-        height: "28mm",
-        contents: {
-            first: 'Cover page',
-            2: 'Second page', // Any page number is working. 1-based index
-            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-            last: 'Last Page'
-        }
-    }
-};
-
-//cambiar en deploy
-// const html = fs.readFileSync("/home/arch-warrior/SoyHenry/ProyectoGrupal/barber-app-backend/helpers/template.html", "utf8");
-const template = path.resolve(__dirname, 'template.html')
-console.log('path', template)
-const html = fs.readFileSync(template, 'utf-8');
-
-const document =  {
-  html: html,
-  data:{
-    datesProducts: datesProducts,
-    idPurchaseOrder: idPurchaseOrder,
-    transaction_amount: transaction_amount,
-    date_approved:date_approved
-  } ,
-  // path: path.join(__dirname, './pdf/Archivo.pdf'),
-  path: path.resolve(__dirname, 'pdf', 'Archivo.pdf'), 
-  type: "",
-};
-
-pdf.create(document, options)
-  .then((res) => {
-    console.log("Respuesta de creacion--->",res);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-  
-
 
 const transporter =  nodemailer.createTransport({
             host: "smtp.gmail.com",
@@ -93,10 +43,6 @@ const info = await transporter.sendMail({
 from: '"Compra realizada con exito 😎" <barberapphenry@gmail.com>', // sender address
   to: `${email}`,
   subject: "Pago realizado ✔", // Subject line
-  attachments: [{
-    filename: 'Archivo.pdf',
-    path: path.resolve(__dirname, 'pdf', 'Archivo.pdf'), 
-    contentType: 'application/pdf'
   }],
   
  //text: "HOLAAA", // plain text body
