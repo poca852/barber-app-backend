613b52920fe4fd39e70840b73efc8751a4c1e6fa
david cuspoca <cuspoca3d@gmail.com>
2022-06-09 11:41:13 -0600

    se le puso una bandera a date (all)
    con este desde el front el administrador puede traer todas las citas sin
    importar su estado

--- controllers/date.js
+++ controllers/date.js
@@ -114,9 +114,36 @@ const addDate = async (req = request, res = response) => {
 };
 
 const getDates = async (req = request, res = response, next) => {
-  const { date, state = true } = req.query;
+  const { date, state = true, all = false } = req.query;
 
   try {
+
+    if(all){
+      const allDates = await DateModel.findAll({
+        include: [
+          {
+            model: ServiceModel,
+            attributes: ["name"],
+            through: {
+              attributes: [],
+            },
+          },
+          {
+            model: UserModel,
+            attributes: ["name", "id", "email"],
+          },
+          {
+            model: EmployeeModel,
+          },
+        ],
+      })
+
+      return res.status(200).json({
+        ok: true,
+        allDates
+      })
+    }
+
     const allDates = await DateModel.findAll({
       where: {
         state
